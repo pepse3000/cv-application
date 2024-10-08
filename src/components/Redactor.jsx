401c9369ee1b2fd2769ca0ee59f9a302fc081a2b
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-10-08 23:17:07 +0300

    feat: add basic redactor component

---
+++ src/components/Redactor.jsx
@@ -0,0 +1,14 @@
+import RedactorContacts from "./RedactorContacts";
+
+function Redactor() {
+
+
+  return (
+    <div className="redactor">
+      <h1>Redactor</h1>
+      <RedactorContacts />
+    </div>
+  )
+}
+
+export default Redactor;
