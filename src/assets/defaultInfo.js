55de503873f0fc27ec567b175e675c936b4b3f32
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-10-08 23:15:24 +0300

    feat: create default cv information

---
+++ src/assets/defaultInfo.js
@@ -0,0 +1,50 @@
+export const defaultInfo = {
+  contacts: {
+    github: "github.com/username",
+    site: "username.com",
+    mail: "username@email.com",
+    number: "+0 000 000 0000"
+  },
+  experience: [
+    {
+      date: "2016.09" + " - " + "Present",
+      companyName: "Company Name",
+      description: "Experience designing Android and iOS apps. An understanding of layout, typography and visual hierarchy. An understanding balance the needs of editorial, the business, and the user. Experience designing for responsive web platforms."
+    },
+  ],
+  education: [
+    {
+      date: "2014" + " - " + "2019",
+      eduName: "Idaho State University",
+      description: "Business Informatics"
+    },
+  ],
+  skills: [
+    {
+      skillName: "React Native",
+      level: 40
+    },
+    {
+      skillName: "HTML",
+      level: 70
+    },
+    {
+      skillName: "JavaScript",
+      level: 70
+    }
+  ],
+  languages: [
+    {
+      langName: "English",
+      level: "C2"
+    },
+    {
+      langName: "Italian",
+      level: "C1",
+    },
+    {
+      langName: "Spanish",
+      level: "B2"
+    }
+  ]
+}
