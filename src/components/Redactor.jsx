e403c120549b61cf85ace14c0189af3286e69be6
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-10-09 19:37:33 +0300

    feat: implement redactors to all fields

--- src/components/Redactor.jsx
+++ src/components/Redactor.jsx
@@ -1,4 +1,9 @@
 import RedactorContacts from "./RedactorContacts";
+import RedactorExperience from "./RedactorExperience";
+import RedactorEducation from "./RedactorEducation";
+import RedactorSkills from "./RedactorSkills";
+import RedactorLanguages from "./RedactorLanguages";
+import RedactorMain from "./RedactorMain";
 
 function Redactor({ info, setInfo }) {
 
@@ -6,7 +11,12 @@ function Redactor({ info, setInfo }) {
   return (
     <div className="redactor">
       <h1>Redactor</h1>
+      <RedactorMain info={info} setInfo={setInfo} />
       <RedactorContacts info={info} setInfo={setInfo} />
+      <RedactorExperience info={info} setInfo={setInfo} />
+      <RedactorEducation info={info} setInfo={setInfo} />
+      <RedactorSkills info={info} setInfo={setInfo} />
+      <RedactorLanguages info={info} setInfo={setInfo} />
     </div>
   )
 }
