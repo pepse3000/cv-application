7611edef9c098b55ca750b210008485471a6524b
pepse3000 <113620477+pepse3000@users.noreply.github.com>
2024-10-09 15:27:39 +0300

    feat: implement collapse to redactor div

--- src/components/RedactorCard.jsx
+++ src/components/RedactorCard.jsx
@@ -1,10 +1,31 @@
+import { useState, useRef } from "react";
+
 function RedactorCard({ name, children }) {
+  const [open, setOpen] = useState(false);
+  const contentRef = useRef(null);
+
+  const toggleCollapse = () => {
+    setOpen(!open);
+  }
+
   return (
     <div className="card">
       <h2>
         {name}
+        <button
+          className={open ? "active" : null}
+          onClick={toggleCollapse}
+        />
       </h2>
-      {children}
+      <div
+        ref={contentRef}
+        style={{
+          maxHeight: open ? `${contentRef.current.scrollHeight}px` : "0px",
+          overflow: "hidden",
+          transition: "max-height 0.3s ease-in-out"
+        }}>
+        {children}
+      </div>
     </div>
   )
 }
